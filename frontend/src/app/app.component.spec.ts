79dfa3d581daad5881367f082432c8aabb10aa97
ErinNPeters <erin.nicole.peters@gmail.com>
2023-07-31 16:28:05 -0500

    all CLI unit tests now pass

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './angular-material.module';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [AngularMaterialModule, RouterTestingModule],
+      declarations: [AppComponent, NavMenuComponent],
     })
   );
 
